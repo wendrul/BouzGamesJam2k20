8cfafd02397d4727683a8f490f78846c062d7b05
Miguel <sexyexoss@gmail.com>
2020-07-08 19:04:00 +0200

    C sharp Champion Inventory

---
+++ BouzGamesJam/Assets/InventorySlot.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems; // Required when using Event data.
+using TMPro;
+
+public class InventorySlot : MonoBehaviour
+{
+
+    public GameObject item;
+    public GameObject itemTable;
+    public TextMeshProUGUI myText;
+    private bool firstInstance = true;
+
+    public int itemstacks;
+
+    public void Start()
+    {
+
+    }
+    private void Update()
+    {
+        myText.SetText(itemstacks.ToString());
+    }
+    public void InstanceObject()
+    {
+        if (itemstacks > 0)
+        {
+            itemstacks--;
+            if (firstInstance == false)
+            {
+            }
+            if (firstInstance == true)
+            {
+//                item.GetComponent<Material>().AddStack();
+                Instantiate(item, new Vector3(itemTable.transform.position.x, itemTable.transform.position.y, item.transform.position.z), Quaternion.identity);
+                firstInstance = false;
+            }
+        }
+    }
+}
--- BouzGamesJam/Assets/Ressources/Scripts/MoveSystem.cs
+++ BouzGamesJam/Assets/Ressources/Scripts/MoveSystem.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MoveSystem : MonoBehaviour
 {
-    private bool snap;
+	public TextMeshPro myText;
+	private bool snap;
     private bool moving;
     private bool over;
     private bool snapInv;
@@ -17,27 +19,16 @@ public class MoveSystem : MonoBehaviour
     private Vector3 resetPosition;
     private Transform runePosition;
     private Transform materialPosition;
-    public GameObject inventory;
-    public float gridoffsetx;
-    public float gridoffsety;
-    RectTransform rt;
-    //public GameObject correctForm;
-    float height;
-    float width;
-    float inventoryWidth;
-    float inventoryHeight;
-    //public GameObject correctForm;
+	public  int stacks;
+
     private void Start()
     {
-        inventoryWidth= inventory.GetComponent<SpriteRenderer>().bounds.size.x;
-        inventoryHeight= inventory.GetComponent<SpriteRenderer>().bounds.size.y;
-        width = GetComponent<SpriteRenderer>().bounds.size.x;
-        height = GetComponent<SpriteRenderer>().bounds.size.y;
         resetPosition = this.transform.localPosition;
-        rt = (RectTransform)inventory.transform;
     }
-    private void Update()
+    void Update()
     {
+		myText.SetText(stacks.ToString());
+
        if (moving)
         {
             Vector3 mousePos;
@@ -46,16 +37,10 @@ public class MoveSystem : MonoBehaviour
          
             this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, this.gameObject.transform.localPosition.z);
         }
-        if (snap && moving == false)
-        {
-            this.transform.position = new Vector3(runePosition.transform.position.x, runePosition.transform.position.y, this.transform.position.z);
-        }
-
-        if (snapInv && moving == false)
-        {
-           // this.transform.position = new Vector3(materialPosition.transform.position.x, materialPosition.transform.position.y, this.transform.position.z);
-            this.transform.position = new Vector3(materialPosition.transform.position.x - inventoryWidth / 2 + width / 2 + gridoffsetx, materialPosition.transform.position.y + inventoryHeight / 2 - height / 2 - gridoffsety, this.transform.position.z);
-        }
+		if (snap && moving == false)
+		{
+			this.transform.position = new Vector3(runePosition.transform.position.x, runePosition.transform.position.y, this.transform.position.z);
+		}
     }
 
     private void OnMouseOver()
@@ -75,19 +60,18 @@ public class MoveSystem : MonoBehaviour
             this.transform.Find("Memo").gameObject.SetActive(false);
         }
     }
-    private void OnMouseDown()
+    public void OnMouseDown()
     {
         if (Input.GetMouseButtonDown(0))
         {
+
             Vector3 mousePos;
             mousePos = Input.mousePosition;
-            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos); 
             startPosX = mousePos.x - this.transform.localPosition.x;
             startPosY = mousePos.y - this.transform.localPosition.y;
-
             moving = true;
-        }
+		}
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -99,13 +83,6 @@ public class MoveSystem : MonoBehaviour
                 runePosition = collision.gameObject.transform;
                 //this.transform.parent = collision.gameObject.transform;
         }
-
-        if (collision.tag == "Inventory")
-
-        {
-            snapInv = true;
-            materialPosition = collision.gameObject.transform;
-        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -113,11 +90,6 @@ public class MoveSystem : MonoBehaviour
         if (collision.tag == "Rune")
         {
             snap = false;
-        }
-        if (collision.tag == "Inventory")
-
-        {
-            snapInv = false;
         }
     }
     private void OnMouseUp()
