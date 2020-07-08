ffb5d6836ba76e6d1a23090839de6df3a2f187fc
Miguel <sexyexoss@gmail.com>
2020-07-08 02:09:23 +0200

    Assigning Material to Rune

--- BouzGamesJam/Assets/Ressources/Scripts/MoveSystem.cs
+++ BouzGamesJam/Assets/Ressources/Scripts/MoveSystem.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class MoveSystem : MonoBehaviour
 {
-
+    private bool snap;
     private bool moving;
 
     private float startPosX;
     private float startPosY;
     private Vector3 resetPosition;
-    public GameObject correctForm;
+    private Transform runePosition;
+    //public GameObject correctForm;
 
 
     private void Start()
@@ -25,8 +26,13 @@ public class MoveSystem : MonoBehaviour
             Vector3 mousePos;
             mousePos = Input.mousePosition;
             mousePos = Camera.main.ScreenToWorldPoint(mousePos);
-
+         
             this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, this.gameObject.transform.localPosition.z);
+        }
+        if (snap && moving == false)
+        {
+            Debug.Log("x: " + runePosition.transform.position.x + "y: " + runePosition.transform.position.y);
+            this.transform.position = new Vector3(runePosition.transform.position.x, runePosition.transform.position.y, this.transform.position.z);
         }
     }
     private void OnMouseDown()
@@ -44,17 +50,29 @@ public class MoveSystem : MonoBehaviour
         }
     }
 
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.tag == "Rune")
+        {
+                snap = true;
+                runePosition = collision.gameObject.transform;
+                //this.transform.parent = collision.gameObject.transform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Rune")
+        {
+            snap = false;
+        }
+    }
     private void OnMouseUp()
     {
+        
         moving = false;
 
-        // if object isnt drag on 1 of the four element reset position
-        if (Mathf.Abs(this.transform.localPosition.x - correctForm.transform.localPosition.x) <= 0.5f &&
-            Mathf.Abs(this.transform.localPosition.y - correctForm.transform.localPosition.y) <= 0.5f)
-        {
-            this.transform.localPosition = new Vector3(correctForm.transform.localPosition.x, correctForm.transform.localPosition.y, correctForm.transform.localPosition.z);
-        }
-        else
-            this.transform.localPosition = new Vector3(resetPosition.x, resetPosition.y, resetPosition.z);
     }
 }
